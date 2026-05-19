3a5e9b0c4afc1a481fbece97ecc59469fdc0dea7
Samit Bhisikar <samitbhisikar16@gmail.com>
2026-05-19 23:45:51 +0530

    feat:Added utils,api responses fix:db errors,express bugs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import dotenv from "dotenv"
 import connectDB from "./db/db";
+import { error } from "three";
 
 dotenv.config({
     path: "./.env"
 });
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running on port ${process.env.PORT || 8000}`)
+    })
+})
+
+.catch((error=>{
+    console.log("DB connection failed!",error)
+}))
 
 /*
 import express from "express";
