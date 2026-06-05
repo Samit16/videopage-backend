f1f2fda3325b71eba9aefe7f9bf1891f62d0c3e6
Samit Bhisikar <samitbhisikar16@gmail.com>
2026-06-05 22:57:01 +0530

    feat added watch history endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -331,6 +331,57 @@ const getUserChannelCoverImage=asyncHandler(async(req,res)=>{
         .json(new ApiResponse(200,channel[0],"Channel cover image fetched successfully",null))
 })
 
+
+const getWatchHistory=asyncHandler(async(req,res)=>{
+    const user=await User.aggregate([
+        {
+            $match:{
+                _id:new mongoose.Types.ObjectId(req.user?._id)
+
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1,
+                                        _id:0
+                                    }
+                                },
+                                {
+                                    $addFields:{
+                                        owner:{
+                                            $first:"$owner" 
+                                        }
+                                    }
+                                }
+                            ]
+                    }
+                }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,user[0]?.watchHistory || [],"User watch history fetched successfully",null))
+})
+
 export {
     registerUser,
     loginUser,
@@ -340,5 +391,6 @@ export {
     getCurrentUser,
     updateAccountSettings,
     updateUserCoverImage,
-    getUserChannelCoverImage
+    getUserChannelCoverImage,
+    getWatchHistory
 }
